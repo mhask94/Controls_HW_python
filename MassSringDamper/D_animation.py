b77d2701ae18fabbb95f2c23e535e961a8ca955a
mhask94 <32270030+mhask94@users.noreply.github.com>
2018-01-17 15:10:00 -0700

    Create D_animation.py

---
+++ MassSringDamper/D_animation.py
@@ -0,0 +1,111 @@
+import matplotlib.pyplot as plt
+import matplotlib.patches as mpatches
+import numpy as np
+import D_param as P
+
+
+class D_animation:
+    '''
+        Create Mass Spring Damper animation
+    '''
+    def __init__(self):
+        self.flagInit = True                  # Used to indicate initialization
+        self.fig, self.ax = plt.subplots()    # Initializes a figure and axes object
+        self.handle = []                      # Initializes a list object that will
+                                              # be used to contain handles to the
+                                              # patches and line objects.
+        self.length=P.length
+        self.width=P.width
+        plt.axis([P.l_lim, P.u_lim, P.l_lim, P.u_lim]) # Change the x,y axis limits
+        plt.plot([P.l_lim, 0], [P.u_lim, 0],'k--')    # Draw ground
+
+        # Draw pendulum is the main function that will call the functions:
+        # drawCart, drawCircle, and drawRod to create the animation.
+    def drawAll(self, u):
+        # Process inputs to function
+        x = u[0]        # Horizontal position of cart, m
+        #theta = u[1]   # Angle of pendulum, rads
+
+        self.drawMass(x)
+        self.drawSpring(x)
+        self.drawDamper(x)
+        self.ax.axis('equal') # This will cause the image to not distort
+
+        # After each function has been called, initialization is over.
+        if self.flagInit == True:
+            self.flagInit = False
+
+    def drawMass(self, u):
+        # Process inputs to function
+        x = u[0]  # x coordinate
+        y = P.gap      # y coordinate
+        xy = (x, y)     # Bottom left corner of rectangle
+
+        # When the class is initialized, a Rectangle patch object will be
+        # created and added to the axes. After initialization, the Rectangle
+        # patch object will only be updated.
+        if self.flagInit == True:
+            # Create the Rectangle patch and append its handle
+            # to the handle list
+            self.handle.append(mpatches.Rectangle(xy,
+                P.length,P.length, fc = 'blue', ec = 'black'))
+            self.ax.add_patch(self.handle[0]) # Add the patch to the axes
+        else:
+            self.handle[0].set_xy(xy)         # Update patch
+
+    def drawSpring(self, u):
+        # Process inputs to function
+        x = u[0]   # x position of cube, m
+        h = P.length*.15
+        dh = P.length*.1
+        xl = P.l_lim
+        dx = (x-P-l_lim)/3.0
+        ddx = dx/6.0
+        points_x = [xl,xl+dx,xl+dx+.5*ddx,xl+dx+1.5*ddx,xl+dx+2.5ddx,xl+dx+3.5*ddx,xl+dx+4.5*ddx,xl+dx+5.5*ddx,xl+2*dx,x]
+        points_y = [h,h,h+dh,h-dh,h+dh,h-dh,h+dh,h-dh,h,h]
+
+        # When the class is initialized, a line object will be
+        # created and added to the axes. After initialization, the
+        # line object will only be update
+        if self.flagInit == True:
+            # Create the line object and append its handle
+            # to the handle list.
+            line, =self.ax.plot(points_x, points_y, lw=5, c='blue')
+            self.handle.append(line)
+            self.flagInit=False
+        else:
+            self.handle[0].set_xdata(X)   # Update the line
+            self.handle[0].set_ydata(Y)
+
+    def drawDamper(self, u):
+        # Process inputs to function
+        theta = u[0]   # angle of arm, rads
+
+        X = [0, self.length*np.cos(theta)]  # X data points
+        Y = [0, self.length*np.sin(theta)]  # Y data points
+
+        # When the class is initialized, a line object will be
+        # created and added to the axes. After initialization, the
+        # line object will only be update
+        if self.flagInit == True:
+            # Create the line object and append its handle
+            # to the handle list.
+            line, =self.ax.plot(X, Y, lw=5, c='blue')
+            self.handle.append(line)
+            self.flagInit=False
+        else:
+            self.handle[0].set_xdata(X)   # Update the line
+            self.handle[0].set_ydata(Y)
+
+
+# Used see the animation from the command line
+if __name__ == "__main__":
+
+    simAnimation = armAnimation()    # Create Animate object
+    theta = 0.0*np.pi/180                 # Angle of arm, rads
+    simAnimation.drawArm([z, theta, 0, 0])  # Draw the arm
+    #plt.show()
+    # Keeps the program from closing until the user presses a button.
+    print('Press key to close')
+    plt.waitforbuttonpress()
+    plt.close()
