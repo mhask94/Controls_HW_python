2b154e9b1571cb0be718880dd2c14f0ff5317ce7
mhask94 <32270030+mhask94@users.noreply.github.com>
2018-01-17 17:13:47 -0700

    Working D_sim.py file

--- MassSringDamper/hw_a/D_sim.py
+++ MassSringDamper/hw_a/D_sim.py
@@ -3,9 +3,9 @@ import numpy as np
 import sys
 sys.path.append('..')  # add parent directory
 import D_param as P
-from signalGenerator import signalGenerator
+# from signalGenerator import signalGenerator
 from D_animation import D_animation
-from plotData import plotData
+# from plotData import plotData
 import matplotlib.patches as patches
 
 # instantiate reference input classes
@@ -18,15 +18,21 @@ import matplotlib.patches as patches
 animation = D_animation()
 
 t = P.t_start  # time starts at t_start
+z = P.x0;
 while t < P.t_end:  # main simulation loop
     # set variables
+    if t < 6:
+        z = z - .08
+    if t >= 6:
+        z = z + .08
+
     # r = reference.square(t)
     # theta = thetaRef.sin(t)
     # tau = tauRef.sawtooth(t)
     # update animation
     # state = [theta[0], 0.0]
-    animation.drawArm(state)
-    dataPlot.updatePlots(t, r, state, tau)
+    animation.drawAll([z])
+    #dataPlot.updatePlots(t, r, state, tau)
 
     t = t + P.t_plot  # advance time by t_plot
     plt.pause(0.1)
