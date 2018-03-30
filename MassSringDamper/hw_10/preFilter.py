cb0a04f1a656f200b360b4d144d174c495cc082b
Unknown <mhaskell_9@hotmail.com>
2018-03-29 18:35:09 -0600

    Modified HW 10 for no overshoot
    This response looks good

--- MassSringDamper/hw_10/preFilter.py
+++ MassSringDamper/hw_10/preFilter.py
@@ -4,12 +4,22 @@ class LSFControl:
         self.u1 = 0.0
         self.e1 = 0.0
 
-        # use these for a = 2.75
-        self.b0 = 0.03323
-        self.b1 = 0.03323
-        self.a1 = -0.9335
+        # # use these for a = 2.75 (slow response)
+        # self.b0 = 0.03323
+        # self.b1 = 0.03323
+        # self.a1 = -0.9335
 
-        # # use these for a = 15
+        # use these for a = 7 (no peak)
+        self.b0 = 0.08046
+        self.b1 = self.b0
+        self.a1 = -0.8391
+
+        # # use these for a = 8 (little peak)
+        # self.b0 = 0.09091
+        # self.b1 = self.b0
+        # self.a1 = -0.8182
+
+        # # use these for a = 15 (really fast response)
         # self.b0 = 0.1579
         # self.b1 = 0.1579
         # self.a1 = -0.6842
